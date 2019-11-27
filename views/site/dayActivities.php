4d6778debc3a7bb595ea9efd8f330d825c69b87c
Daria Diachkova <dariadiachkova@mail.ru>
2019-11-27 11:31:18 +0400

    task3

--- views/site/dayActivities.php
+++ views/site/dayActivities.php
@@ -6,7 +6,8 @@ $this->title = 'События на сегодня';
 ?>
 
 <h1><?= Html::encode($this->title) ?></h1>
-<h2>Свойства
+<div>
+    <h2>Свойства</h2>
     <?php if ($model->main) : ?>
         <p>Событие блокирует остальные ></p>
     <?php endif; ?>
@@ -19,7 +20,18 @@ $this->title = 'События на сегодня';
     <?php else : ?>
         <p>Событие c <?= date("d.m.Y", $model->startDay) ?> по <?= date("d.m.Y", $model->endDay) ?></p>
     <?php endif; ?>
+</div>
+<div>
+    <h2>Файлы</h2>
+    <?php
+    foreach ($model->uploads as $key => $value) {
+        echo "{$key} => {$value} ";  //TODO вывод файлов как иконок(?)
+    } ?>
+</div>
 
-    <?= Html::a('Вернуться в календарь', ['calendar/view', 'userId' => $user->id], ['class' => 'calendar-link']) ?>
 
-    <button class="btn btn-lg"><?= Html::a('Редактировать событие', ['calendar/#', 'userId' => $user->id], ['class' => 'calendar-link']) ?><button>
+
+
+<?= Html::a('Вернуться в календарь', ['calendar/view', 'userId' => $user->id], ['class' => 'calendar-link']) ?>
+
+<button class="btn btn-lg"><?= Html::a('Редактировать событие', ['calendar/#', 'userId' => $user->id], ['class' => 'calendar-link']) ?><button>
